be5486a4f9dcd114d3be6e743a286ecd8885f759
Jake H <73356928+AcedDown@users.noreply.github.com>
2022-08-13 03:10:38 +0100

    Update activity.jsx

--- pages/api/util/activity.jsx
+++ pages/api/util/activity.jsx
@@ -2,6 +2,6 @@ import fetch from 'isomorphic-unfetch';
 
 export default async function Activity(req, res) {
     res.json(
-        await (await fetch('https://linkcord.js.org/api/v3/user/451444721089380373')).json()
+        await (await fetch('https://linkcord.js.org/api/v3/user/591978052381310978')).json()
     );
 };
